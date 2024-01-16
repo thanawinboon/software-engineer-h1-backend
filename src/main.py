9c07a8480abc6f861f0029e59e5c461314544c0c
thanawinboon <pokepat27@gmail.com>
2024-01-17 00:19:35 +0700

    updated list of dependencies

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from src.auth.router import router as auth_router
+from src.database.database import create_db_and_tables
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth")
 
+create_db_and_tables()
 
 @app.get("/")
 def hello():
