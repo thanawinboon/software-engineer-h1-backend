e1f4568a83dd544de62f023136000e25d2561154
thanawinboon <pokepat27@gmail.com>
2024-01-16 22:56:47 +0700

    added required dependencies

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
 
 from src.auth.router import router as auth_router
 
@@ -7,14 +6,6 @@ app = FastAPI()
 app.include_router(auth_router, prefix="/auth")
 
 
-SECRET_KEY = "your-secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-# OAuth2 Password Bearer Token
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-@app.get("/helloworld")
+@app.get("/")
 def hello():
     return {"msg": "yay"}
