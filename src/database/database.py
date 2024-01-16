8c643479e1fe0468893157ee4f00a54e499b7e3b
thanawinboon <pokepat27@gmail.com>
2024-01-17 00:23:14 +0700

    fixed database credentials

--- src/database/database.py
+++ src/database/database.py
@@ -5,10 +5,10 @@ from sqlmodel import SQLModel, create_engine
 
 load_dotenv()
 DB_USER = os.environ.get("DB_USER", "postgres")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "password")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "postgres")
 DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_PORT = os.environ.get("DB_PORT", 5432)
-DB_NAME = os.environ.get("DB_NAME", "backend")
+DB_NAME = os.environ.get("DB_NAME", "postgres")
 
 db_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(db_url)
