4c8210761466d4004f94e6ee51973f7a6d2cac43
thanawinboon <pokepat27@gmail.com>
2024-01-18 03:34:42 +0700

    added print statements

--- src/requests/router.py
+++ src/requests/router.py
@@ -8,8 +8,7 @@ router = APIRouter()
 auth_handler = AuthHandler()
 
 @router.get("/list", status_code=200)
-def view_requests(user_id=Depends(auth_handler.auth_wrapper)):    
-    print(user_id)
+def view_requests(user_id=Depends(auth_handler.auth_wrapper)):
     if not user_id:
         raise HTTPException(status_code=401, detail="Unauthorized")
     
@@ -17,13 +16,13 @@ def view_requests(user_id=Depends(auth_handler.auth_wrapper)):
 
 @router.post("/send", status_code=201)
 def make_request(request_details: LeaveRequest, user_id=Depends(auth_handler.auth_wrapper)):
-    print(request_details)
-    
     selected_user = get_user_by_id(user_id)
     if not selected_user:
+        print("User does not exist")
         raise HTTPException(status_code=400, detail="User does not exist")
     
     if request_details.leave_duration < 1:
+        print("Leave duration should be at least 1 day")
         raise HTTPException(status_code=400, detail="Leave duration should be at least 1 day")
     
     created_request = create_request(selected_user, request_details)
