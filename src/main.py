87b346f68b48e025a5906e5f683711e811c20cf6
thanawinboon <pokepat27@gmail.com>
2024-01-17 23:42:19 +0700

    imported request module

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from src.auth.router import router as auth_router
+from src.requests.router import router as requests_router
 from src.database.database import create_db_and_tables
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth")
+app.include_router(requests_router)
 
 create_db_and_tables()
 
