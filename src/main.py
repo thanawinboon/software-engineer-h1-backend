92b418326892b371e5d1768a78e09773c22070ee
thanawinboon <pokepat27@gmail.com>
2024-01-18 03:33:35 +0700

    updated cors setting

--- src/main.py
+++ src/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.router import router as auth_router
 from src.requests.router import router as requests_router
 from src.database.database import create_db_and_tables
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router, prefix="/auth")
 app.include_router(requests_router, prefix="/leave-request")
 
